2b5303af6e5e9985caafb6bd5bef97b580a39c0d
RobinWu <robin.wu.cn@gmail.com>
2011-10-11 20:57:57 +0800

    add runner

---
+++ runner/down.sh
@@ -0,0 +1,8 @@
+wget http://www.riaidea.com/html5/runner/images/grasslands_grid_s.png
+wget http://www.riaidea.com/html5/runner/images/gatling_01.png
+wget http://www.riaidea.com/html5/runner/images/gatling_02.png
+wget http://www.riaidea.com/html5/runner/images/soldier_01.png
+wget http://www.riaidea.com/html5/runner/images/font.png
+wget http://www.riaidea.com/html5/runner/images/other.png
+wget http://www.riaidea.com/html5/runner/sounds/grasslands.ogg
+wget http://www.riaidea.com/html5/runner/sounds/grasslands.mp3
